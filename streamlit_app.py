64f559477e883c946ae424f808da0f5410e136aa
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2024-01-15 18:59:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,23 +9,23 @@ st.markdown("*Streamlit* is **really** ***cool***.")
 
 # db_username = "Jane"
 
-urls = [
-"https://github.com/", 
-"https://www.w3schools.com/", 
-"https://streamlit.io/"
-]
-
-
-def job1():
-	for i, url in enumerate(urls, start=1):
-		r = requests.get(url)
-		print(f"{i}, {url}", r.status_code)
-		st.write(f"{i}, {url}", r.status_code)
-		time.sleep(10)
-schedule.every(60).seconds.do(job1)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+#urls = [
+#"https://github.com/", 
+#"https://www.w3schools.com/", 
+#"https://streamlit.io/"
+#]
+#
+#
+#def job1():
+#	for i, url in enumerate(urls, start=1):
+#		r = requests.get(url)
+#		print(f"{i}, {url}", r.status_code)
+#		st.write(f"{i}, {url}", r.status_code)
+#		time.sleep(10)
+#schedule.every(60).seconds.do(job1)
+#while True:
+#    schedule.run_pending()
+#    time.sleep(1)
 
 #def job():
 #	r = requests.get('https://www.w3schools.com/')
