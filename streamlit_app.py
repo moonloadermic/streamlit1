53d9e4dda69429850762ca97d008fd32d46c1b71
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2024-01-15 16:47:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,9 @@
 import time
 import streamlit as st
 import requests
-"""
-## NO
 
-## Web scraping on Streamlit Cloud with Selenium
 
-[![Source](https://img.shields.io/badge/View-Source-.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
-
-This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
-
-Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
-"""
-
-st.title("Test Selenium")
+st.title("hello")
 st.markdown("*Streamlit* is **really** ***cool***.")
 
 r = requests.get('https://w3schools.com')
