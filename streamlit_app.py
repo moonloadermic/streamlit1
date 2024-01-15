ec2dacb7f0e095633a4e2b2b71ada9193cdf38b0
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2024-01-15 18:43:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,18 @@ st.markdown("*Streamlit* is **really** ***cool***.")
 
 # db_username = "Jane"
 
+urls = [
+"https://github.com/", 
+"https://www.w3schools.com/", 
+"https://streamlit.io/"
+]
+
+r = requests.get(url)
+
+for i, url in enumerate(urls, start=1):
+    print(f"{i}, {url}, r.status_code")
+	time.sleep(8)
+
 #def job():
 #	r = requests.get('https://www.w3schools.com/')
 #	return r.status_code
