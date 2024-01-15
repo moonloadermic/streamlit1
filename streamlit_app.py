799f5a3a573c68722cfd8459ba6fbea43520d85f
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2024-01-15 17:17:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,11 @@ import requests
 st.title("hello")
 st.markdown("*Streamlit* is **really** ***cool***.")
 
+# db_username = "Jane"
+
 r = requests.get('https://www.w3schools.com/')
 
-st.write("url", r.status_code)
+st.write("website status: ", r.status_code)
 st.markdown(r.status_code)
 
 
