591899c8e6ab03271caa0d0184820a1c88b8d595
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2024-01-15 17:36:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,15 @@ st.markdown("*Streamlit* is **really** ***cool***.")
 
 # db_username = "Jane"
 
-def job():
-	r = requests.get('https://www.w3schools.com/')
-	return r.status_code
+#def job():
+#	r = requests.get('https://www.w3schools.com/')
+#	return r.status_code
 	
-schedule.every(60).seconds.do(job)
+#schedule.every(60).seconds.do(job)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+#while True:
+#    schedule.run_pending()
+#    time.sleep(1)
 
 
 
